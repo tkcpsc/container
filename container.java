e572c3ff55399a59262b36cbb0c28e465def4b96
tkcpsc <kudey@chapman.edu>
2023-06-03 14:47:26 -0700

    Created add(), remove(), toString.

--- container.java
+++ container.java
@@ -1,18 +1,52 @@
+
 public class container {
     private char[] arr;
-    private int size;
+    private int conSize;
+    private int arrSize;
 
     // Default Constructor.
     public container() {
-
+        arr = new char[5];
+        conSize = 0;
+        arrSize = 5;
     }
     
     // Overloaded Constructor.
-    public container(char[] arr) {
-
+    public container(char[] arr, int conSize, int arrSize) {
+        this.arr = arr;
+        this.conSize = conSize;
+        this.arrSize = arrSize;
     }
 
     public boolean isEmpty() {
-        return (size==0);
+        return (conSize==0);
     }    
+    public void add(char in) {
+        // Appends (in) into array while also incrementing conSize.
+        arr[conSize++] = in;
+    }
+    public boolean remove(char in) {
+        if (isEmpty()) return (false);
+        for (int i = 0; i < conSize; i++) {
+            if (arr[i] == in){
+                conSize--;
+                arr[i] = arr[conSize];
+                return true;
+            }
+        }
+        
+        return false;
+    }   
+    
+    
+    public String toString(){
+        String ret = "";
+        for (int i = 0; i < conSize; i++) {
+           ret += arr[i] + ", "; 
+        }
+        // ret += " conSize: " + conSize;
+        // ret += ", arrSize: " + arrSize;
+        return ret;
+    }
+    
 }
--- mainprogram.java
+++ mainprogram.java
@@ -4,6 +4,16 @@
 public class mainprogram {
 
     public static void main(String[] args) {
+        container c1 = new container();
+        c1.add('A');
+        System.out.println(c1);
+        c1.add('B');
+        System.out.println(c1);
+        c1.remove('B');
+        System.out.println(c1);
+        c1.remove('A');
+        System.out.println(c1);
         
+
     }
 }

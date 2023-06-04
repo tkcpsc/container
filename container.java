ed77462d459aa0feb8405a5c798392b2fd262431
Thomas Kudey <thomskudey@gmail.com>
2023-06-03 18:21:26 -0700

    converted array to tree based

--- container.java
+++ container.java
@@ -1,52 +1,29 @@
+import java.util.TreeSet;
 
 public class container {
-    private char[] arr;
-    private int conSize;
-    private int arrSize;
+    private TreeSet<Character> tree = new TreeSet<>();
 
     // Default Constructor.
     public container() {
-        arr = new char[5];
-        conSize = 0;
-        arrSize = 5;
+
     }
     
     // Overloaded Constructor.
-    public container(char[] arr, int conSize, int arrSize) {
-        this.arr = arr;
-        this.conSize = conSize;
-        this.arrSize = arrSize;
+    public container(TreeSet<Character> tree) {
+        this.tree = tree;
     }
 
     public boolean isEmpty() {
-        return (conSize==0);
+        return (tree.size()==0);
     }    
     public void add(char in) {
-        // Appends (in) into array while also incrementing conSize.
-        arr[conSize++] = in;
+        tree.add(in);
     }
     public boolean remove(char in) {
-        if (isEmpty()) return (false);
-        for (int i = 0; i < conSize; i++) {
-            if (arr[i] == in){
-                conSize--;
-                arr[i] = arr[conSize];
-                return true;
-            }
-        }
-        
-        return false;
+        return tree.remove(in);
     }   
-    
-    
     public String toString(){
-        String ret = "";
-        for (int i = 0; i < conSize; i++) {
-           ret += arr[i] + ", "; 
-        }
-        // ret += " conSize: " + conSize;
-        // ret += ", arrSize: " + arrSize;
-        return ret;
+        return tree.toString();
     }
     
 }
